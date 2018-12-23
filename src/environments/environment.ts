b134a98e13894e28d9e5fe330bc7deb25fa5e680
Pascal van Nugteren <pascal@rednose.nl>
2018-12-23 20:01:27 +0100

    Changed failing unit tests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  appId: '1e4fa063311aea5f1b8e1772331cf208',
+  baseUrl: 'http://'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
